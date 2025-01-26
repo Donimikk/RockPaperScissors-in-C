e20e21a16d92cb36abbf361ac04e3a8f217c3204
Dominik Ivanov <ivanov.dominik@student.tuke.sk>
2025-01-26 20:11:54 +0100

    start

---
+++ rps.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<unistd.h>
+
+int main(){
+    printf("\t\nWelcome to ROCK PAPER SCISSORS!");
+    sleep(3);
+    system("cls");
+    printf("Choose rock (r) paper (p) scissors (s): ");
+
+    return 0;
+}
